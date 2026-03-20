981463a6400c48d19fdb5ae3ec6ee66229345e3b
Vladimir Varga <vvarga@ditec.sk>
2026-03-20 19:25:34 +0100

    remove old node backend

--- backend/start.js
+++
@@ -1,9 +0,0 @@
-const {spawn} = require('child_process');
-const express = require('express')
-const app = express()
-
-const port = 6001
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`jetson-sprinkler backend listening at http://localhost:${port}`))
